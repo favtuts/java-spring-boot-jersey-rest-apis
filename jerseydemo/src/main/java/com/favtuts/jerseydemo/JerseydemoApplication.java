ec475b66f7fd6dc419f39deb2462bb2c8392dc7e
favtuts <favtuts@gmail.com>
2022-04-19 20:24:27 +0700

    add full source code for demostration

---
+++ jerseydemo/src/main/java/com/favtuts/jerseydemo/JerseydemoApplication.java
@@ -0,0 +1,13 @@
+package com.favtuts.jerseydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class JerseydemoApplication extends SpringBootServletInitializer {
+	public static void main(String[] args) {
+		new JerseydemoApplication().configure(new SpringApplicationBuilder(JerseydemoApplication.class)).run(args);
+	}
+}
